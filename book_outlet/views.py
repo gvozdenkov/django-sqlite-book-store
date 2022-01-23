a18fa0b94706165cf5d6447df641749af4ba764f
arty <gvozdenkov@gmail.com>
2022-01-23 06:18:02 +0300

    add agregate func

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -2,6 +2,7 @@ from logging.handlers import SYSLOG_UDP_PORT
 from django.forms import SlugField
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.db.models import Avg, Min        #агрегатная функция среднее
 
 from django.http import Http404
 
@@ -10,9 +11,14 @@ from . models import Book
 # Create your views here.
 
 def index(request):
-    all_books = Book.objects.all()
+    all_books = Book.objects.all().order_by("-rating")
+    num_books = all_books.count()
+    avg_rating = all_books.aggregate(Avg("rating"), Min("rating"))      # возвращает словарь rating__avg, rating__min etc
+
     return render(request, "book_outlet/index.html", {
-        "all_books": all_books
+        "all_books": all_books,
+        "total_books": num_books,
+        "average_rating": avg_rating
     })
 
 def book_detail(request, slug):
